4fd883c046904e6be854327ea88461a66f72a43c
Marc-Antoine Duhaime <ma@duhaime.ca>
2024-04-05 15:31:38 -0400

    Layout for demo

--- src/components/ApiCallDemo.vue
+++ src/components/ApiCallDemo.vue
@@ -11,15 +11,39 @@ onMounted(async () => {
   try {
     users.value = await userService.getUsers();
   } catch (error) {
+    // TODO: This is never trigger.  Handled by the service...
     console.log(`ApiCallDemo.vue onMounted() error: ${error}`);
   }
 });
 </script>
 
 <template>
-  <div class="users" v-if="users">
-    <p>{{ users.length }} user(s) found.</p>
-    <p>First user is : {{ users[0].name }}</p>
+  <p v-if="users">{{ users.length }} user(s) found.</p>
+
+  <div class="table-container">
+    <table class="table is-striped is-fullwidth is-narrow">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>City</th>
+          <th>Company</th>
+          <th>Website</th>
+        </tr>
+      </thead>
+      <tbody v-if="users">
+        <tr v-for="user in users">
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.address.city }}</td>
+          <td>{{ user.company.name }}</td>
+          <td>{{ user.website }}</td>
+        </tr>
+      </tbody>
+      <tfoot></tfoot>
+    </table>
   </div>
 </template>
 
